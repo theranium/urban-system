eb2303be00b3139eca4e7e6fc6fde3f7af4d7e27
theranium <hemanume@gmail.com>
2024-11-26 20:24:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("st program")
+print("12+89-1")
+
+print("1st program")
+print(9**0.5*5)
+
+print("2nd program")
+print('9.99 > 9.98, 1000 != 1000.01')
+print('9.99 > 9.98, 1000 '!=' 1000.01')
+
+print("3rd program")
+print(2 + 2 * 2)
+print((2+2) * 2)
+print(2 + 2 * 2 == (2+2) * 2)
+
+print("4th program")
+print('123.456')
